ae4c61ad355c702477150c2070bbd11418fd7211
zoroloco <kcenturion@yahoo.com>
2019-03-27 09:49:01 -0500

    xmms2d now spawned as well and song title now stored in mongo db.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var pathUtil = require('path'),
     Xmms     = require(pathUtil.join(__dirname,'./xmms.js')),
     conf     = require(pathUtil.join(__dirname,'./conf.json')),
     http     = require('http'),
+    mongoloid= require(pathUtil.join(__dirname,'./mongoose/mongoloid.js')),
     https    = require('https');
 
 log.init();
@@ -27,41 +28,50 @@ process.title = conf.title;
 var myXmms    = new Xmms();
 let app       = express();//start the server.
 
-//define process handlers
-process.on('SIGTERM', function() {
-    log.info("Got kill signal. Exiting.");
-    //httpsServer.close();
-    httpServer.close();
-    process.exit();
-});
-
-process.on('SIGINT', function() {
-    log.warn("Caught interrupt signal(Ctrl-C)");
-    //httpsServer.close();
-    httpServer.close();
-    process.exit();
-});
-
-process.on('exit', function(){
-    log.info("server process exiting...");
-});
-
-process.on('uncaughtException', function (err) {
-    let msg="Uncaught Exception ";
-    if( err.name === 'AssertionError' ) {
-        msg += err.message;
-    } else {
-        msg += err;
-    }
-    log.error(msg);
-});
 /*
 let httpsServer = https.createServer(app.get('httpsOptions'),app).listen(app.get('port'),function(){
     log.info(process.title+" server now listening on SSL port:"+httpsServer.address().port);
 });
 */
-let httpServer = http.createServer(app).listen(app.get('httpPort'),function(){
-    log.info(process.title+" server now listening on HTTP port:"+httpServer.address().port);
-});
 
-myXmms.connect();
+mongoloid.init(function(status) {
+    if (status) {
+        let httpServer = http.createServer(app).listen(app.get('httpPort'),function(){
+            log.info(process.title+" server now listening on HTTP port:"+httpServer.address().port);
+        });
+
+        //define process handlers
+        process.on('SIGTERM', function() {
+            log.info("Got kill signal. Exiting.");
+            //httpsServer.close();
+            httpServer.close();
+            process.exit();
+        });
+
+        process.on('SIGINT', function() {
+            log.warn("Caught interrupt signal(Ctrl-C)");
+            //httpsServer.close();
+            httpServer.close();
+            process.exit();
+        });
+
+        process.on('exit', function(){
+            log.info("server process exiting...");
+        });
+
+        process.on('uncaughtException', function (err) {
+            let msg="Uncaught Exception ";
+            if( err.name === 'AssertionError' ) {
+                msg += err.message;
+            } else {
+                msg += err;
+            }
+            log.error(msg);
+        });
+
+        myXmms.connect();
+    }
+    else{
+        log.error('Cannot connect to MongoDB');
+    }
+});
--- xmms.js
+++ xmms.js
@@ -1,10 +1,12 @@
 var pathUtil = require('path'),
     log      = require(pathUtil.join(__dirname,'./logger.js')),
     cp       = require('child_process'),
+    mongoloid= require(pathUtil.join(__dirname,'./mongoose/mongoloid.js')),
+    XmmsModel= require(pathUtil.join(__dirname,'./mongoose/xmms-model.js')),
     _        = require('underscore');
 
-
 var myXmms;
+var myXmms2d;
 
 function Xmms(){
     var self = this;
@@ -18,21 +20,63 @@ function Xmms(){
 
     Xmms.prototype.connect = function() {
         var self = this;
-        myXmms = cp.spawn('/usr/bin/xmms2');
-        myXmms.stdin.setEncoding('utf-8');
+        log.info('Now spawning xmms2d');
+        myXmms2d = cp.spawn('/usr/bin/xmms2d');
 
-        myXmms.on('close', (code) => {
-            log.info('XMMS process closed with code:'+code);
+        setTimeout(function(){
+            log.info('1.5 seconds have passed. now safe to spawn xmms2.');
+            myXmms = cp.spawn('/usr/bin/xmms2');
+
+            myXmms.stdin.setEncoding('utf-8');
+
+            myXmms.on('close', (code) => {
+                log.info('XMMS process closed with code:'+code);
+            });
+
+            myXmms.on('exit', (code) => {
+                log.info('XMMS process exited with code:'+code);
+            });
+
+            myXmms.stdout.on('data', (data) => {
+                //log.info('rx stdout from xmms:'+data.toString());
+            });
+
+            myXmms.stderr.on('data', (err) => {
+                log.error('XMMS received stderr from remote xmms:'+err);
+                Xmms.saveAction(null,err);
+            });
+        },1500);
+
+
+        myXmms2d.stdout.on('data', (data)=>{
+            let dataStr = data.toString();
+            //log.info('XMMS2D received stdout from remote xmms2d:'+dataStr);
+            let mStr = dataStr.substring(dataStr.indexOf('Music-Ken'),dataStr.indexOf('\' (')).replace('Music-Ken/','');
+            log.info('Music Filename:'+mStr);
+            Xmms.saveAction(mStr);
+        });
+    };
+
+    Xmms.saveAction = function(msg,errMsg){
+        if(_.isEmpty(msg)){
+            log.warn('Not saving empty filename!');
+            return;
+        }
+
+        let saveMe = new XmmsModel.model({
+            song_title: msg,
+            error_msg:  errMsg,
+            event_time: new Date()
         });
 
-        myXmms.on('exit', (code) => {
-            log.info('XMMS process exited with code:'+code);
-        });
+        //log.info("Saving:" + JSON.stringify(saveMe));
+
+        mongoloid.save(saveMe);
     };
 
     Xmms.sendCommand = function(req,res){
         var self = this;
-        log.info('XMMS got command:'+req.originalUrl);
+        //log.info('XMMS got command:'+req.originalUrl);
 
         if(req.originalUrl === '/shuffle'){
             let rand = Math.floor(Math.random() * 4848);
@@ -40,20 +84,11 @@ function Xmms(){
         }
         else{
             let cmd = req.originalUrl.replace('/','');
-            log.info('Sending to xmms command:'+cmd);
+            //log.info('Sending to xmms command:'+cmd);
             myXmms.stdin.write(cmd+'\r');
         }
 
-        myXmms.stdout.on('data', (data) => {
-            let dataStr = data.toString();
-            log.info('XMMS received stdout from remote xmms:'+dataStr);
-            res.json({'msg':dataStr}).status(200);
-        });
-
-        myXmms.stderr.on('data', (err) => {
-            log.error('XMMS received stderr from remote xmms:'+err);
-            res.json({'errMsg':err}).status(200);
-        });
+        res.json({'msg':'okie dokie'}).status(200);
     }
 }
 
